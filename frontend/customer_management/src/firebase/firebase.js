e92c7a2cebd5710bc2556bb4fbfce612e22ee33e
Soham03 <38619657+Soham03@users.noreply.github.com>
2024-08-13 16:25:49 +0530

    Update firebase.js

--- frontend/customer_management/src/firebase/firebase.js
+++ frontend/customer_management/src/firebase/firebase.js
@@ -5,13 +5,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDJWu9HPmanavmypT0kPgm3K_byoPSq238",
-  authDomain: "customer-management-d04fb.firebaseapp.com",
-  projectId: "customer-management-d04fb",
-  storageBucket: "customer-management-d04fb.appspot.com",
-  messagingSenderId: "241597215115",
-  appId: "1:241597215115:web:e23f390603294a32b27f8e",
-  measurementId: "G-EWN1WJYVC2"
+
 };
 
 // Initialize Firebase
